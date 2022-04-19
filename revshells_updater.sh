fc0ee8d0be221a738f1616224b28cd03298200d4
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2022-04-19 22:55:03 +0200

    rip brain, this is the correct way.

--- revshells_updater.sh
+++ revshells_updater.sh
@@ -12,9 +12,7 @@ e = [ '|'.join(i.split('|')[1:]).strip() for i in c ]
 [ print(f"# {i.capitalize()}\n" + e[c] + "\n") for c, i in enumerate(d) ] 
 EOF
 echo -e '% rev shells generated from github/mthbernardes/rsg\n' | tee revshells_generated.cheat
-
-# printf "%q" "$(head -n 3 ./rev-shells.cheat | grep -i ' IP:' | sed 's/\$ IP: //g' )"
-echo for\ adaptername\ in\ \$\(for\ i\ in\ \"\$\(ip\ a\ s\ \|\ sort\ -r\ \|\ grep\ -E\ \$\'\[\[:digit:\]\]:\[\ \]\'\)\"\;\ do\ echo\ \$\(echo\ \$i\ \|\ awk\ \'\{print\ \$2\}\'\ \|\ replace\ :\ \'\'\)\;\ done\)\;\ do\ ip\ a\ s\ \$adaptername\ \|\ grep\ \'inet\\\ \'\ \|\ awk\ \'\{print\ \$2\}\'\ \|\ cut\ -d\ \'/\'\ -f\ 1\ \;\ done\ \|\ uniq | tee -a revshells_generated.cheat
-
+grep '^\$ IP:' ./rev-shells.cheat | tee -a revshells_generated.cheat
+grep '^\$ port:' ./listeners.cheat | tee -a revshells_generated.cheat
 python3 /tmp/scrape.py | tee -a revshells_generated.cheat
 rm /tmp/scrape.py /tmp/shells.txt
