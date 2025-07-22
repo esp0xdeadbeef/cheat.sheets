ead4d54e71a056d7bbea9ab27fda4a7f73050512
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-07-22 22:39:21 +0200

    added the variable correctly for ip addresses

--- revshells_updater.sh
+++ revshells_updater.sh
@@ -12,7 +12,7 @@ e = [ '|'.join(i.split('|')[1:]).strip() for i in c ]
 [ print(f"# {i.capitalize()}\n" + e[c] + "\n") for c, i in enumerate(d) ] 
 EOF
 echo -e '% rev shells generated from github/mthbernardes/rsg\n' | tee revshells_generated.cheat
-grep '^\$ IP:' ./rev-shells.cheat | tee -a revshells_generated.cheat
+grep '^\$ ip_target:' ./rev-shells.cheat | tee -a revshells_generated.cheat
 grep '^\$ port:' ./listeners.cheat | tee -a revshells_generated.cheat
 echo "" | tee -a revshells_generated.cheat
 python3 /tmp/scrape.py | tee -a revshells_generated.cheat
