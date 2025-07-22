8c151c5aa4e888c6024c4aa9b07611a912c81d98
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-07-22 23:02:42 +0200

    sudo please just work for once

--- revshells_updater.sh
+++ revshells_updater.sh
@@ -5,15 +5,14 @@ cat > /tmp/scrape.py << EOF
 a = open("/tmp/shells.txt")
 b = a.read()
 a.close()
-b= b.replace('[IPADDR]', '<ip_target>').replace('[PORT]', '<port>').strip()
+b= b.replace('[IPADDR]', '<IP>').replace('[PORT]', '<port>').strip()
 c=[element for element in b.split('\n')]
 d = [ i.split('|')[0].strip() for i in c ]
 e = [ '|'.join(i.split('|')[1:]).strip() for i in c ]
 [ print(f"# {i.capitalize()}\n" + e[c] + "\n") for c, i in enumerate(d) ] 
 EOF
 echo -e '% rev shells generated from github/mthbernardes/rsg\n' | tee revshells_generated.cheat
-grep '^\$ IP:' ./rev-shells.cheat | sed 's/IP/ip_target/g' | tee -a revshells_generated.cheat
-grep '^\$ port:' ./listeners.cheat | tee -a revshells_generated.cheat
+cat ./variables.template | tee -a revshells_generated.cheat
 echo "" | tee -a revshells_generated.cheat
 python3 /tmp/scrape.py | tee -a revshells_generated.cheat
 rm /tmp/scrape.py /tmp/shells.txt
